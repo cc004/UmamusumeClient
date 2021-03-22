5be92f0bbeb0243f4b11a75c2b8b3f1d1ce226fb
qq1176321897 <1176321897@qq.com>
2021-03-22 17:26:58 +0800

    update app ver to 1.2.0

--- Umamusume/SimpleLz4Frame.cs
+++ Umamusume/SimpleLz4Frame.cs
@@ -20,6 +20,8 @@ namespace Umamusume
         private readonly int id;
         private readonly bool crypt;
 
+        private static readonly bool dbg = UmamusumeClient.dbg;
+
         public SimpleLz4Frame(int id, bool crypt = true)
         {
             this.id = id;
@@ -29,11 +31,13 @@ namespace Umamusume
 
         public byte[] Decompress(string src)
         {
+            if (dbg) Console.WriteLine($"decompressing: {src}");
             if (!crypt) return Convert.FromBase64String(src);
             return Convert.FromBase64String(client.PostAsync($"http://localhost:2333/{id}/decompress", new StringContent(src)).Result.Content.ReadAsStringAsync().Result);
         }
         public string Compress(byte[] src)
         {
+            if (dbg) Console.WriteLine($"compressing: {Convert.ToBase64String(src)}");
             if (!crypt) return Convert.ToBase64String(src);
             return client.PostAsync($"http://localhost:2333/{id}/compress", new StringContent(Convert.ToBase64String(src))).Result.Content.ReadAsStringAsync().Result;
         }
