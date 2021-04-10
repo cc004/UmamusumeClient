50f98dbc9208a50248c94fb136fe4d6531aad54b
qq1176321897 <1176321897@qq.com>
2021-04-10 19:33:55 +0800

    add follow

---
+++ Umamusume/Model/AccountContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Umamusume.Model
+{
+    public class AccountContext : DbContext
+    {
+        public static AccountContext context = new AccountContext();
+
+        public DbSet<DBAccount> Account { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=accounts.db");
+    }
+
+    public class DBAccount
+    {
+        public int id { get; set; }
+        public string udid { get; set; }
+        public string authkey { get; set; }
+        public string password { get; set; }
+        public string cards { get; set; }
+        public int cardnum { get; set; }
+        public int viewer_id { get; set; }
+
+    }
+}
--- Umamusume/Program.cs
+++ Umamusume/Program.cs
@@ -2,9 +2,6 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SQLite;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -15,15 +12,13 @@ using Umamusume.Model.Master;
 
 namespace Umamusume
 {
-    internal class Program
+    public class Program
     {
-        private static SQLiteConnection conn;
-        private static SQLiteConnection conn2;
-        private static readonly SortedDictionary<int, string> cards = new SortedDictionary<int, string>(MasterContext.Instance
+        private static readonly SortedDictionary<int, string> cards = new (MasterContext.Instance
             .TextData.Where(c => c.Category == 75 && c.Index > 30000)
             .ToDictionary(c => (int)c.Index, c => c.Text));
 
-        private static void SaveTo(UmamusumeClient client, SQLiteConnection conn)
+        private static void SaveTo(UmamusumeClient client)
         {
             string pwd = Utils.GenRandomPassword();
             client.RetryRequest(new ChangeNameRequest
@@ -32,6 +27,8 @@ namespace Umamusume
             });
             client.PublishTransition(pwd);
             client.Account.extra.password = pwd;
+
+            /*
             const string qstr = "insert into accounts (udid, authkey, password, cardnum, viewer_id, {0}) values (@udid, @authkey, @password, @cardnum, @viewer_id, {1})";
 
             SQLiteCommand cmd = new SQLiteCommand(string.Format(qstr, string.Join(",", cards.Select(s => s.Value).Select(s => s[1..s.IndexOf("]")]).Select(s => $"`{s}`")), string.Join(",", Enumerable.Range(1, cards.Count).Select(i => $"@c{i}"))), conn);
@@ -45,6 +42,20 @@ namespace Umamusume
             cmd.Parameters.Add("viewer_id", DbType.Int32).Value = client.Account.ViewerId;
             lock (conn)
                 cmd.ExecuteNonQuery();
+            */
+
+
+            AccountContext.context.Account.Add(new DBAccount
+            {
+                authkey = client.Account.Authkey,
+                cardnum = client.Account.extra.support_cards.Count,
+                cards = string.Join("<br/>", client.Account.extra.support_cards.Select(pair => $"[{cards[pair.Key]},{pair.Value}]")),
+                password = client.Account.extra.password,
+                udid = client.Account.Udid.ToString(),
+                viewer_id = client.Account.ViewerId
+            });
+
+            AccountContext.context.SaveChanges();
 
             var invalid = client.Account.extra.support_cards.FirstOrDefault(p => !cards.ContainsKey(p.Key));
             if (invalid.Key > 30000)
@@ -53,8 +64,65 @@ namespace Umamusume
             }
         }
 
-        private static int gachaid = 0;
+        private static int _gachaid = 0;
         private static readonly object gachaidlock = new object();
+
+        public static void RegisterOnce(UmamusumeClient client)
+        {
+
+            try
+            {
+                client.ResetAccount();
+                client.Signup();
+                //var client = new UmamusumeClient(JsonConvert.DeserializeObject<Account>(File.ReadAllText("account.json")));
+                //File.WriteAllText("account.json", JsonConvert.SerializeObject(client.Account));
+                client.StartSession();
+                client.Login();
+                client.RetryRequest(new TutorialSkipRequest());
+
+                client.StartSession();
+                client.Login();
+                var presents = client.ReceivePresents().reward_summary_info.add_item_list;
+                int _gachaid;
+
+                lock (gachaidlock)
+                {
+                    if (Program._gachaid == 0)
+                    {
+                        GachaInfoList[] gachas = client.RetryRequest(new GachaLoadRequest()).data.gacha_info_list;
+                        Program._gachaid = gachas.Where(gachas => gachas.id / 10000 == 3).Select(gachas => gachas.id).Max();
+                    }
+                    _gachaid = Program._gachaid;
+                }
+
+                while (client.FCoin >= 1500)
+                    client.Gacha(_gachaid);
+
+                const int ssrticketId = 114;
+
+                for (int i = presents.SingleOrDefault(i => i.item_id == ssrticketId)?.number ?? 0; i > 0; --i)
+                    client.Gacha(20002, 1, ssrticketId, i);
+
+                Console.WriteLine($"{client.LogPrefix} gacha get = {client.Account.extra.support_cards.Count}");
+                int count = client.Account.extra.support_cards.Sum(p => p.Value);
+
+                if (client.Account.extra.support_cards.TryGetValue(30028, out var val) && val > 2)
+                    SaveTo(client);
+                else
+                {
+                    client.RetryRequest(new FriendFollowRequest()
+                    {
+                        friend_viewer_id = 250278690
+                    });
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{client.LogPrefix} {e}");
+            }
+        }
+
         private static void RegisterTask(int id)
         {
             UmamusumeClient client = new UmamusumeClient(new SimpleLz4Frame(id))
@@ -63,55 +131,7 @@ namespace Umamusume
             };
             while (true)
             {
-                try
-                {
-                    client.Signup();
-                    //var client = new UmamusumeClient(JsonConvert.DeserializeObject<Account>(File.ReadAllText("account.json")));
-                    //File.WriteAllText("account.json", JsonConvert.SerializeObject(client.Account));
-                    client.StartSession();
-                    client.Login();
-                    client.RetryRequest(new TutorialSkipRequest());
-
-                    client.StartSession();
-                    client.Login();
-                    var presents = client.ReceivePresents().reward_summary_info.add_item_list;
-                    int _gachaid;
-
-                    lock (gachaidlock)
-                    {
-                        if (gachaid == 0)
-                        {
-                            GachaInfoList[] gachas = client.RetryRequest(new GachaLoadRequest()).data.gacha_info_list;
-                            gachaid = gachas.Where(gachas => gachas.id / 10000 == 3).Select(gachas => gachas.id).Max();
-                        }
-                        _gachaid = gachaid;
-                    }
-
-                    while (client.FCoin >= 1500)
-                        client.Gacha(_gachaid);
-
-                    const int ssrticket_id = 114;
-
-                    for (int i = presents.SingleOrDefault(i => i.item_id == ssrticket_id)?.number ?? 0; i >0; --i)
-                        client.Gacha(20002, 1, ssrticket_id, i);
-
-                    Console.WriteLine($"[Thread #{id}] gacha get = {client.Account.extra.support_cards.Count}");
-                    int count = client.Account.extra.support_cards.Sum(p => p.Value);
-
-                    if (client.Account.extra.support_cards.Count > 5)
-                    {
-                        SaveTo(client, conn);
-                    }
-                    else if (client.Account.extra.support_cards.Count == 5)
-                    {
-                        SaveTo(client, conn2);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"[Thread #{id}] {e}");
-                }
-                client.ResetAccount();
+                RegisterOnce(client);
             }
         }
 
@@ -144,9 +164,12 @@ namespace Umamusume
 
         private static void Main(string[] args)
         {
-            Test();
+            //Test();
             ThreadPool.SetMaxThreads(512, 512);
             ThreadPool.SetMinThreads(128, 128);
+            AccountContext.context.Database.EnsureCreated();
+
+            /*
             conn = new SQLiteConnection("data source=accounts.db");
             conn.Open();
             conn2 = new SQLiteConnection("data source=accounts2.db");
@@ -186,6 +209,7 @@ namespace Umamusume
 
             }
 
+            */
             for (int i = 0; i < 64; ++i)
             {
                 int j = i;
